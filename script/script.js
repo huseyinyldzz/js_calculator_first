b5057813b48f38a66f54c77fb21f386463de084b
huseyinyldzz <hsynyldz.586@gmail.com>
2023-06-02 12:21:29 +0300

    update

---
+++ script/script.js
@@ -0,0 +1,32 @@
+const screen = document.querySelector(`h1`);
+
+const buttons = document.querySelectorAll(`button`);
+
+const clearButton = document.getElementById("clrButton");
+
+
+
+
+buttons.forEach((button) => {
+        if(button.classList.length ===0) {
+            button.addEventListener(`click`, function(e) {
+            console.log(e);
+            screen.textContent = e;
+            button.value;
+
+        }else if (button.classList.contains(`operators`)){
+            button.addEventListener(`click`, function(e) {
+                console.log(e);
+                screen.textContent = e;
+                button.value;
+
+        }}
+} );
+
+
+function clearAll() {
+    screen.textContent= "12";
+}
+
+clearButton.addEventListener(`click`, clearAll);
+
